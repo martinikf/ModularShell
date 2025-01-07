2001f7098b926c9b546d287541d760da133af09f
Filip Martiník <martinik.filip01@gmail.com>
2025-01-08 00:38:12 +0100

    Shared dependent

--- Scripts/ModeDlls.ps1
+++ Scripts/ModeDlls.ps1
@@ -2,14 +2,31 @@
 $debug = "bin/Debug/net8.0"
 $wwwrootModules = "../ModularShell/wwwroot/Modules"
 
-# Create the destination folders if they don't exist
+# Create the destination root folder if it doesn't exist
 New-Item -ItemType Directory -Path $wwwrootModules -Force
 
-# Move the DummyDependency.dll
-Move-Item -Path "../DummyDependency/$debug/DummyDependency.dll" -Destination "$wwwrootModules/DummyDependency" -Force
+# Define the list of DLLs and their corresponding source project folders
+$dllsToCopy = @(
+    @{ Name = "DummyDependency"; ProjectFolder = "../DummyDependency" }
+    @{ Name = "RazorCLDependent"; ProjectFolder = "../RazorCLDependent" }
+    @{ Name = "RazorCLSharedDependent"; ProjectFolder = "../RazorCLSharedDependent" }
+    @{ Name = "RazorCLStandalone"; ProjectFolder = "../RazorCLStandalone" }
+)
 
-# Move the RazorCLDependent.dll
-Move-Item -Path "../RazorCLDependent/$debug/RazorCLDependent.dll" -Destination "$wwwrootModules/RazorCLDependent" -Force
+# Loop through the list of DLLs
+foreach ($dllInfo in $dllsToCopy) {
+    # Construct the source DLL path
+    $dllPath = Join-Path -Path $dllInfo.ProjectFolder -ChildPath "$debug/$($dllInfo.Name).dll"
 
-# Move the RazorCLStandalone.dll
-Move-Item -Path "../RazorCLStandalone/$debug/RazorCLStandalone.dll" -Destination "$wwwrootModules/RazorCLStandalone" -Force
+    # Construct the destination path
+    $destinationPath = Join-Path -Path $wwwrootModules -ChildPath $dllInfo.Name
+
+    # Create the destination directory if it doesn't exist
+    New-Item -ItemType Directory -Force -Path $destinationPath
+
+    # Copy the DLL
+    Write-Host "Copying $($dllInfo.Name).dll from $($dllPath) to $($destinationPath)"
+    Copy-Item -Path $dllPath -Destination $destinationPath -Force
+}
+
+Write-Host "DLLs copied successfully!"
